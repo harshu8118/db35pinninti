3584c1f138b456faaf9545603b17012eb38fbedc
Harshika <pinnintiharshika@gmail.com>
2021-11-10 21:54:13 -0600

    final commit

--- controllers/Lion.js
+++ controllers/Lion.js
@@ -15,9 +15,25 @@ exports.Lion_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: Lion detail: ' + req.params.id);
 };
 // Handle Lion create on POST.
-exports.Lion_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: Lion create POST');
-};
+exports.Lion_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Lion();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"Lion_type":"goat", "cost":12, "size":"large"}
+    document.Lion_type = req.body.Lion_type;
+    document.size = req.body.size;
+    document.cost = req.body.cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
 // Handle Lion delete form on DELETE.
 exports.Lion_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Lion delete DELETE ' + req.params.id);
