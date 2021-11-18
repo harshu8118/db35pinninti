4adb6f9528786f41fd2464c3e2f1fc73cccd77df
Harshika <pinnintiharshika@gmail.com>
2021-11-17 21:02:47 -0600

    done

--- controllers/Lion.js
+++ controllers/Lion.js
@@ -43,9 +43,17 @@ exports.Lion_create_post = async function(req, res) {
     }
    };
 // Handle Lion delete form on DELETE.
-exports.Lion_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: Lion delete DELETE ' + req.params.id);
-};
+exports.Lion_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Lion.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+  }; 
 // Handle Lion update form on PUT.
 exports.Lion_update_put = async function(req, res) { 
     console.log(`update on id ${req.params.id} with body 
@@ -77,4 +85,55 @@ exports.Lion_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
-   
+   exports.Lion_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Lion.findById( req.query.id)
+    res.render('Liondetail',
+   { title: 'Lion Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   // Handle building the view for creating a Lion.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Lion_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Lioncreate', { title: 'Lion Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   // Handle building the view for updating a Lion.
+// query provides the id
+exports.Lion_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Lion.findById(req.query.id)
+    res.render('lionupdate', { title: 'Lion Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle a delete one view with id from query
+exports.Lion_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Lion.findById(req.query.id)
+    res.render('Liondelete', { title: 'Lion Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/Lion.js
+++ routes/Lion.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var Lion_controlers = require('../controllers/Lion');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('Lion', { title: 'Express' });
 });
+
+router.get('/detail', Lion_controlers.Lion_view_one_Page);
+/* GET create Lion page */
+router.get('/create', Lion_controlers.Lion_create_Page);
+/* GET create update page */
+router.get('/update', Lion_controlers.Lion_update_Page);
+/* GET create Lion page */
+router.get('/delete', Lion_controlers.Lion_delete_Page);
 
 module.exports = router;
