38ca3645f805e756872f9d0a7f6c3e1e18eff73f
Harshika <pinnintiharshika@gmail.com>
2021-11-19 00:15:56 -0600

    lab

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
 
 const mongoose = require("mongoose");
 var db = mongoose.connection;
@@ -73,14 +88,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", function () {
   console.log("Connection to DB succeeded");
 });
-
-
 app.use("/", indexRouter);
 app.use("/Lion", LionRouter);
 app.use("/users", usersRouter);
@@ -88,7 +107,13 @@ app.use("/selector", selectorRouter);
 app.use("/addmods", addmodsRouter);
 app.use("/Lion", LionRouter);
 app.use('/resource', resourceRouter);
-
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
