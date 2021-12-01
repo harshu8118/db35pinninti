efa094e7357f150f0f88522f83edc170f00a29ba
Harshika <pinnintiharshika@gmail.com>
2021-12-01 15:55:23 -0600

    add

--- models/Lion.js
+++ models/Lion.js
@@ -1,8 +1,15 @@
-mongoose = require("mongoose");
-const LionSchema = mongoose.Schema({
-Lion_type: String,
-size: String,
-cost: Number
-})
-module.exports = mongoose.model("Lion",
+const mongoose = require("mongoose") 
+const LionSchema = mongoose.Schema({ 
+ Brand: {
+    type: String,
+    minlength: 9
+},
+ cost: Number, 
+ size: {
+    type: String,
+    minlength: 5
+}, 
+}) 
+ 
+module.exports = mongoose.model("Lion", 
 LionSchema)
--- routes/Lion.js
+++ routes/Lion.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var Lion_controlers = require('../controllers/Lion');
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
+/* GET update Lion page */ 
+// router.get('/update', Lion_controlers.costume_update_Page); 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Lion', { title: 'Express' });
-});
+router.get('/', Lion_controlers.Lion_view_all_Page);
 
 router.get('/detail', Lion_controlers.Lion_view_one_Page);
 /* GET create Lion page */
-router.get('/create', Lion_controlers.Lion_create_Page);
+router.get('/create', secured, Lion_controlers.Lion_create_Page);
 /* GET create update page */
-router.get('/update', Lion_controlers.Lion_update_Page);
+router.get('/update', secured, Lion_controlers.Lion_update_Page);
 /* GET create Lion page */
-router.get('/delete', Lion_controlers.Lion_delete_Page);
+router.get('/delete', secured, Lion_controlers.Lion_delete_Page);
 
 module.exports = router;
